39703687aed006d42b65756e1a10d6a522d42bc7
rebecacalvoquintero <rebeca.quintero@orkestro.com>
2020-04-04 12:24:03 +0700

    navigation completed

---
+++ Profije.js
@@ -0,0 +1,14 @@
+import {Text, View} from 'react-native';
+
+import React from 'react';
+
+const Profile = () => {
+  return (
+    <View>
+      <Text>Complete your profile!</Text>
+      {/* name, age, gender */}
+    </View>
+  );
+};
+
+export default Profile;
